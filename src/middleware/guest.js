cec60d6e4bded1b31e5b1dc27d09ad9b7a4ef360
Sam O <sam.odukoya@jdplc.com>
2021-03-31 01:36:20 +0100

    modify guest middleware

--- src/middleware/guest.js
+++ src/middleware/guest.js
@@ -1,14 +1,6 @@
 export default function guest ({ next, to, store }) {
-  console.log(store.getters['Auth/isAuthenticated'])
   if (store.getters['Auth/isAuthenticated']) {
     return next({ name: 'promo.list' })
   }
   return next()
-  // store.dispatch('User/getUser')
-  //   .then(() => {
-  //     next()
-  //   })
-  //   .catch(() => {
-  //     next({ name: 'auth1.sign-in', query: { redirect_to: to.fullPath } })
-  //   })
 }
